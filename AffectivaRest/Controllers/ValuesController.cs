0ca96c5187843e07f2e922df97443d067eb74097
Christina Kayastha <ckayastha@vistaprint.com>
2016-03-06 08:04:12 -0500

    server side controller working

--- AffectivaRest/Controllers/ValuesController.cs
+++ AffectivaRest/Controllers/ValuesController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Affdex;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace AffectivaRest.Controllers
 {
@@ -24,15 +25,52 @@ namespace AffectivaRest.Controllers
             return "value";
         }
 
+        private class ProcessOutput
+        {
+            internal bool processed;
+            public bool detectSmile;
+            public bool anger;
+            public bool attention;
+            public bool browFurrow;
+            public bool browRaise;
+            public bool lipPress;
+            public bool contempt;
+            public bool disgust;
+            public bool chinRaise;
+            public bool engagement;
+            public bool eyeClosure;
+            public bool fear;
+            public bool gender;
+            public bool glasses;
+            public bool innerBrowRaise;
+            public bool joy;
+            public bool lipCornerDepressor;
+            public bool lipPucker;
+            public bool lipSuck;
+            public bool mouthOpen;
+            public bool noseWrinkle;
+            public bool sadness;
+            public bool smile;
+            public bool smirk;
+            public bool surprise;
+            public bool upperLipRaise;
+            public bool valence;
+
+            public ProcessOutput(bool detectSmile) {
+                this.processed = false;
+                this.detectSmile = detectSmile;
+            }
+        }
+
         private class ProcessListener : Affdex.ProcessStatusListener
         {
             private PhotoDetector pd;
-            private AsyncCallback callback;
+            private ProcessOutput po;
 
-            public ProcessListener(PhotoDetector pd)
+            public ProcessListener(PhotoDetector pd, ProcessOutput po)
             {
                 this.pd = pd;
-                //this.callback = callback;
+                this.po = po;
                 pd.setProcessStatusListener(this);
             }
 
@@ -43,12 +81,35 @@ namespace AffectivaRest.Controllers
 
             public void onProcessingFinished()
             {
-                pd.getDetectSmile();   
-                throw new NotImplementedException();
+                po.anger = pd.getDetectAnger();
+                po.attention = pd.getDetectAttention();
+                po.browFurrow = pd.getDetectBrowFurrow();
+                po.browRaise = pd.getDetectBrowRaise();
+                po.chinRaise = pd.getDetectChinRaise();
+                po.contempt = pd.getDetectContempt();
+                po.disgust = pd.getDetectDisgust();
+                po.engagement = pd.getDetectEngagement();
+                po.eyeClosure = pd.getDetectEyeClosure();
+                po.fear = pd.getDetectFear();
+                po.gender = pd.getDetectGender();
+                po.glasses = pd.getDetectGlasses();
+                po.innerBrowRaise = pd.getDetectInnerBrowRaise();
+                po.joy = pd.getDetectJoy();
+                po.lipCornerDepressor = pd.getDetectLipCornerDepressor();
+                po.lipPress = pd.getDetectLipPress();
+                po.lipPucker = pd.getDetectLipPucker();
+                po.lipSuck = pd.getDetectLipSuck();
+                po.mouthOpen = pd.getDetectMouthOpen();
+                po.noseWrinkle = pd.getDetectNoseWrinkle();
+                po.sadness = pd.getDetectSadness();
+                po.smile = pd.getDetectSmile();
+                po.smirk = pd.getDetectSmirk();
+                po.surprise = pd.getDetectSurprise();
+                po.upperLipRaise = pd.getDetectUpperLipRaise();
+                po.valence = pd.getDetectValence();
+                po.processed = true;
             }
         };
-
-        public delegate bool CallBack(int hwnd, int lParam);
 
         // POST api/values
         public string Post([FromBody]dynamic value)
@@ -67,18 +128,13 @@ namespace AffectivaRest.Controllers
 
             String classifierPath = "C:\\Program Files\\Affectiva\\Affdex SDK\\data";
             pd.setClassifierPath(classifierPath);
-
-            //public static string Report(int hwnd, int lParam)
-            //{
-            //    return "true";
-            //}
-
-            //CallBack myCallBack = new CallBack(EnumReportApp.Report);
-            //EnumWindows(myCallBack, 0);
-
-            new ProcessListener(pd);
-
-            pd.setDetectSmile(true);
+            
+            var po = new ProcessOutput(false);
+            var pl = new ProcessListener(pd, po);
+            
+            pd.setDetectAllEmojis(true);
+            pd.setDetectAllEmotions(true);
+            pd.setDetectAllExpressions(true);
 
             pd.start();
 
@@ -86,7 +142,13 @@ namespace AffectivaRest.Controllers
 
             //pd.stop();
 
-            return "NON";
+            while (true)
+            {
+                if (po.processed) {
+
+                    return JsonConvert.SerializeObject(po);
+                }
+            }
         }
 
         // PUT api/values/5
